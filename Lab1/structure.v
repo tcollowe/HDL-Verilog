c91a583b960196458b2d8e0fcf3907da8723d804
Tyler Storr <ts8250@cs.ship.edu>
2021-03-04 01:50:34 +0000

    added structure

---
+++ Lab1/structure.v
@@ -0,0 +1,10 @@
+module structure (A, B, C, Q);
+    input A, B, C;
+    output Q;
+    wire w1, w2, w3, w4;
+    and and1(w1, A, B);
+    or or1(w2, B, C);
+    and and2(w3, C, B);
+    and and3(w4, w2, w3);
+    or or2(Q, w1, w4);
+endmodule
