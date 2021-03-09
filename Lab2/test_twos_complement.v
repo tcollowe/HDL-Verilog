b61e6717f851578e55e8c1747a03eed087324618
Tyler Storr <ts8250@cs.ship.edu>
2021-03-09 17:33:11 +0000

    part 2 works! woo!

---
+++ Lab2/test_twos_complement.v
@@ -0,0 +1,18 @@
+`default_nettype none
+`timescale 1ns/1ns
+
+module test_twos_complement;
+    reg [3:0] inp;
+    wire [3:0] out;
+
+    twos_complement #(.WIDTH(4)) uut(inp, out);
+
+    initial begin
+        inp = 0001;
+        #1
+        if(out != 4'b1111) $display("Expecting %b, got %b", inp, out);
+    
+    $display("Test Two's finished");
+    $finish;
+    end
+endmodule
