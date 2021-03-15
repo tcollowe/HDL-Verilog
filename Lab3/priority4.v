d4797f1eba43c248083db0051d4889c376a60826
Tyler Storr <ts8250@cs.ship.edu>
2021-03-15 16:18:22 +0000

    fixed bug in priority4

--- Lab3/priority4.v
+++ Lab3/priority4.v
@@ -1,9 +1,19 @@
+`default_nettype none
 module priority4 (in, out, valid);
 	input wire[3:0] in;
-	output reg[1:0] out;
+	output wire[1:0] out;
 	output wire valid;
 
+	wire t1, t2, t3, t4, t5;
+	not n1(t1, in[2]);
+	and a1(t2, t1, in[1]);
+	or o1(t3, t2, in[3]);
+	or o2(t4, in[3], in[2]);
+	or o3(t5, t4, in[1], in[0]);
 
 
+	assign out = {t4, t3};
+	// assign out[1] <= t4;
+	assign valid = t5;
 	
 endmodule
--- Lab3/test_priority4.v
+++ Lab3/test_priority4.v
@@ -1,32 +1,33 @@
 `default_nettype none
 `timescale 1ns/1ns
 
-module test_priority4;
-    wire [3:0] in;
-	wire [1:0] sel;
-	wire out;
+module test_priority4();
+    reg [3:0] in;
+	wire [1:0] out;
+	wire valid;
 
-    priority4 uut(in, sel, out);
+    priority4 uut(in, out, valid);
 
     initial begin
         in = 4'b0000;
-        if(valid != 1'b0) $display("Err at: in = %b, sel = %b, output should be = , got ouput = %b", in, sel, in, out);
+        #1
+        if(valid != 1'b0) $display("Err at: in = %b, valid = %b, output should be = , got ouput = %b", in, valid, in, out);
 		#1
 
 		in = 4'b1000;
-        if((valid != 1'b1) || (out != 2'b00)) $display("Err at: in = %b, sel = %b, output should be = %b, got ouput = %b", in, sel, in, out);
+        if((valid != 1'b1) || (out != 2'b00)) $display("Err at: in = %b, valid = %b, output should be = %b, got ouput = %b", in, valid, in, out);
 		#1
 
 		in = 4'b0100;
-        if((valid != 1'b1) || (out != 2'b01)) $display("Err at: in = %b, sel = %b, output should be = %b, got ouput = %b", in, sel, in, out);
+        if((valid != 1'b1) || (out != 2'b01)) $display("Err at: in = %b, valid = %b, output should be = %b, got ouput = %b", in, valid, in, out);
 		#1
 
 		in = 4'b0010;
-        if((valid != 1'b1) || (out != 2'b10)) $display("Err at: in = %b, sel = %b, output should be = %b, got ouput = %b", in, sel, in, out);
+        if((valid != 1'b1) || (out != 2'b10)) $display("Err at: in = %b, valid = %b, output should be = %b, got ouput = %b", in, valid, in, out);
 		#1
 
 		in = 4'b0001;
-        if((valid != 1'b1) || (out != 2'b11)) $display("Err at: in = %b, sel = %b, output should be = %b, got ouput = %b", in, sel, in, out);
+        if((valid != 1'b1) || (out != 2'b11)) $display("Err at: in = %b, valid = %b, output should be = %b, got ouput = %b", in, valid, in, out);
     end
 
 
