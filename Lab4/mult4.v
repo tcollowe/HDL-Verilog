db50d3f0f0277f2df22470808773f5b3cafb29c2
Zac Baker <zb4403@cs.ship.edu>
2021-03-16 04:47:52 -0400

    Mult4 complete and tested
    Mult4 complete and tested, passed tests with edge cases

--- Lab4/mult4.v
+++ Lab4/mult4.v
@@ -1,22 +1,14 @@
-`default_nettype none
-module mult4 (a, b, c);
-    input [3:0] a;
-    input [3:0] b;
+module mult4(a, b, c);
+    input [3:0] a, b;
     output [7:0] c;
 
-    wire a0, a1, a2, a3;
-    wire b0, b1, b2, b3;
-
-    assign a0 = a [0];
-    assign a1 = a [1];
-    assign a2 = a [2];
-    assign a3 = a [3];
-
-    assign b0 = b [0];
-    assign b1 = b [1];
-    assign b2 = b [2];
-    assign b3 = b [3];
+    wire [6:0] p0, p1, p2, p3;
     
-    
+    assign p0 = (b [0]) ? a : 0;
+    assign p1 = (b [1]) ? a << 1 : 0;
+    assign p2 = (b [2]) ? a << 2 : 0;
+    assign p3 = (b [3]) ? a << 3 : 0;
+
+    assign c = p0 + p1 + p2 + p3;
 
 endmodule
--- Lab4/test_mult4.v
+++ Lab4/test_mult4.v
@@ -2,13 +2,31 @@
 `timescale 1ns/1ns
 
 module test_mult4();
-    wire a, b, c;
+    reg [3:0] a, b;
+    wire [7:0] c;
 
     mult4 uut1(a, b, c);
+
     initial begin
-        
+        $display("Starting test.\n");
+
+        a = 4'b1010;
+        b = 4'b1100;
+        #1;
+        $display("a = %b (%d)\nb = %b (%d)\nc = %b (%d)\n", a, a, b, b, c, c);
+
+        a = 4'b1111;
+        b = 4'b1111;
+        #1;
+        $display("a = %b (%d)\nb = %b (%d)\nc = %b (%d)\n", a, a, b, b, c, c);
+
+        a = 4'b0000;
+        b = 4'b0000;
+        #1;
+        $display("a = %b (%d)\nb = %b (%d)\nc = %b (%d)\n", a, a, b, b, c, c);
+
+        $display("Test complete.");
+
     end
-
-
 
 endmodule
