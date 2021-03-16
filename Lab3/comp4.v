5a223baf15469e07b6e555fc52529ef15788f07b
Zac Baker <zb4403@cs.ship.edu>
2021-03-16 04:59:59 -0400

    Optimized comp4 code
    Removed like 75% of the code because it was unnecessary

--- Lab3/comp4.v
+++ Lab3/comp4.v
@@ -5,27 +5,15 @@ module comp4(
         output lt
 );
 
-        wire a0, a1, a2, a3;
-        wire b0, b1, b2, b3;
         wire e0, e1, e2, e3;
 
-        assign a0 = a [0];
-        assign a1 = a [1];
-        assign a2 = a [2];
-        assign a3 = a [3];
-
-        assign b0 = b [0];
-        assign b1 = b [1];
-        assign b2 = b [2];
-        assign b3 = b [3];
-
-        assign e0 = (a0 && b0) || (!a0 && !b0);
-        assign e1 = (a1 && b1) || (!a1 && !b1);
-        assign e2 = (a2 && b2) || (!a2 && !b2);
-        assign e3 = (a3 && b3) || (!a3 && !b3);
+        assign e0 = (a [0] && b [0]) || (!a [0] && !b [0]);
+        assign e1 = (a [1] && b [1]) || (!a [1] && !b [1]);
+        assign e2 = (a [2] && b [2]) || (!a [2] && !b [2]);
+        assign e3 = (a [3] && b [3]) || (!a [3] && !b [3]);
 
         assign eq = e0 && e1 && e2 && e3;
 
-        assign lt = (eq == 1'b1) ? 0 : !((a3 && !b3) + (e3 && a2 && !b2) + (e3 && e2 && a1 && !b1) + (e3 && e2 && e1 && a0 && !b0));
+        assign lt = (eq == 1'b1) ? 0 : !((a [3] && !b [3]) + (e3 && a [2] && !b [2]) + (e3 && e2 && a [1] && !b [1]) + (e3 && e2 && e1 && a [0] && !b [0]));
 
 endmodule
