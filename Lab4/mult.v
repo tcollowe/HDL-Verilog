396bf58e30f34e88cac5d790e1fbba07d53e1fab
Zac Baker <zb4403@cs.ship.edu>
2021-03-16 21:53:49 -0400

    Continued work on mult.v
    Continued on Collette's work on mult.v

--- Lab4/mult.v
+++ Lab4/mult.v
@@ -1,7 +1,29 @@
 `default_nettype none
-module mult (a, b, c);
-    input wire[3:0] a;
-    input wire[3:0] b;
-    output wire[7:0] c;
+module multiplier(a, b, c);
+    parameter WIDTH = 0;
+    input wire [WIDTH-1:0] a;
+    input wire [WIDTH-1:0] b;
+    output wire [2*WIDTH-1:0] c;
+
+    genvar i;
+    generate
+        for (i = 0; i < WIDTH; i = i + 1) begin:mult
+            wire [2*WIDTH-1:0] partial;
+            assign partial = (b [i]) ? a << i : 0;
+        end
+    endgenerate
+
+    wire [2*WIDTH-1:0] product;
+    assign product = 0;
+
+    genvar j;
+    generate
+        for (j = 0; j < WIDTH; j = j + 1) begin:sum
+            if (j == 0) assign product = 0;
+            assign product = product + mult[j].partial;
+        end
+    endgenerate
+
+    assign c = product;
     
 endmodule
--- Lab4/test_mult.v
+++ Lab4/test_mult.v
@@ -2,11 +2,16 @@
 `timescale 1ns/1ns
 
 module test_mult();
-    wire a, b, c;
+    parameter TEST_WIDTH = 4; // also have to change a and b bit counts, those couldn't use this macro
+    reg [TEST_WIDTH-1:0] a, b;
+    wire [2*TEST_WIDTH-1:0] c;
 
-    mult uut1(a, b, c);
+    multiplier #(.WIDTH(TEST_WIDTH)) uut1(a, b, c);
     initial begin
-        
+        a = 4'b1010;
+        b = 4'b1100;
+        #1;
+        $display("\t\tWIDTH = %d\n\t\ta = %b (%d)\n\t\tb = %b (%d)\n\t\tc = %b (%d)\n", TEST_WIDTH, a, a, b, b, c, c);
     end
 
 
