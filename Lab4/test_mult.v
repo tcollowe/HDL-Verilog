bf35b641d14cfa0e17137ceebd65557dc50778fa
Zac Baker <zb4403@cs.ship.edu>
2021-03-17 03:35:01 -0400

    Mult working, failing some edge cases
    Failing edge cases when width is 1

--- Lab4/test_mult.v
+++ Lab4/test_mult.v
@@ -2,16 +2,20 @@
 `timescale 1ns/1ns
 
 module test_mult();
-    parameter TEST_WIDTH = 4; // also have to change a and b bit counts, those couldn't use this macro
+    parameter TEST_WIDTH = 1; // also have to change a and b bit counts, those couldn't use this macro
     reg [TEST_WIDTH-1:0] a, b;
     wire [2*TEST_WIDTH-1:0] c;
 
     multiplier #(.WIDTH(TEST_WIDTH)) uut1(a, b, c);
     initial begin
-        a = 4'b1010;
-        b = 4'b1100;
+        $display("Starting test.\n");
+
+        a = 1'b1;
+        b = 1'b1;
         #1;
-        $display("\t\tWIDTH = %d\n\t\ta = %b (%d)\n\t\tb = %b (%d)\n\t\tc = %b (%d)\n", TEST_WIDTH, a, a, b, b, c, c);
+        $display("WIDTH = %d\na = %b (%d)\nb = %b (%d)\nc = %b (%d)\n", TEST_WIDTH, a, a, b, b, c, c);
+
+        $display("Test complete.");
     end
 
 endmodule
